b3cc0a97b2a47cbaf06e0f36301c2819a72370b8
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-15 18:52:44 +0100

    Update Navbar component with logo image and title; integrate Navbar into Home page layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@public/logo.svg";
+import Logo from "@/public/logo.png";
 
 export function Navbar() {
     return (
         <div className="flex py-5 items-center justify-between">
-            <Link href="/">
+            <Link href="/" className="flex items-center gap-2">
                 <Image src={Logo} alt='Logo' className="size-10" />
+                <h4 className="text-3xl font-semibold">Cal<span className="text-orange-500">Sajeeb</span></h4>
             </Link>
+
         </div>
     )
 }
