615df408f5175a8c20bfa0288da3312e6d391d45
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-17 21:44:45 +0100

    Add username uniqueness validation to onboarding schema

--- app/lib/zodSchemas.ts
+++ app/lib/zodSchemas.ts
@@ -1,6 +1,36 @@
+import { conformZodMessage } from '@conform-to/zod';
 import { z } from 'zod';
 
 export const onboardingSchema = z.object({
     fullName: z.string().min(3).max(150),
     userName: z.string().min(3).max(150).regex(/^[a-zA-Z0-9-]+$/, { message: 'Only letters, numbers, and hyphens are allowed' }),
 });
+
+export function onboardingSchemaValidation(options?: {
+    isUsernameUnique: () => Promise<boolean>
+}) {
+    return z.object({
+        userName: z.string().min(3).max(150).regex(/^[a-zA-Z0-9-]+$/, { message: 'Only letters, numbers, and hyphens are allowed' }).pipe(
+            z.string().superRefine((_, ctx) => {
+                if (typeof options?.isUsernameUnique !== 'function') {
+                    ctx.addIssue({
+                        code: 'custom',
+                        message: conformZodMessage.VALIDATION_UNDEFINED,
+                        fatal: true,
+                    });
+                    return;
+                }
+
+                return options.isUsernameUnique().then((isUnique) => {
+                    if (!isUnique) {
+                        ctx.addIssue({
+                            code: 'custom',
+                            message: 'Username is already taken',
+                        })
+                    }
+                })
+            })
+        ),
+        fullName: z.string().min(3).max(150),
+    })
+}
