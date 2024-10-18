8c7d1a60646e132fddf6ea2f10f64c8fe065caf8
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-18 17:32:09 +0100

    Add user data retrieval in DashboardLayout to redirect to onboarding if username is missing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,9 +10,30 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { signOut } from "../lib/auth";
 import { requireUser } from "../lib/hooks";
+import prisma from "../lib/db";
+import { redirect } from "next/navigation";
+
+async function getData(userId: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            userName: true,
+        },
+    });
+
+    if (!data?.userName) {
+        return redirect("/onboarding");
+    }
+
+    return data;
+}
 
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
     const session = await requireUser();
+
+    const data = await getData(session.user?.id as string);
 
     return (
         <>
