10e5243d31c8d8e8c1764281c9444499d220ee95
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-18 16:58:16 +0100

    Enhance onboarding action to validate unique username and redirect to dashboard

--- app/actions.ts
+++ app/actions.ts
@@ -3,13 +3,26 @@
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hooks";
 import { parseWithZod } from "@conform-to/zod";
-import { onboardingSchema } from "./lib/zodSchemas";
+import { onboardingSchemaValidation } from "./lib/zodSchemas";
+import { redirect } from "next/navigation";
 
 export async function OnboardingAction(prevState: any, formData: FormData) {
     const session = await requireUser();
 
-    const submission = parseWithZod(formData, {
-        schema: onboardingSchema,
+    const submission = await parseWithZod(formData, {
+        schema: onboardingSchemaValidation({
+            async isUsernameUnique() {
+                const existingUsername = await prisma.user.findUnique({
+                    where: {
+                        userName: formData.get("userName") as string,
+                    },
+                });
+
+                return !existingUsername;
+            }
+        }),
+
+        async: true,
     });
 
     if (submission.status !== "success") {
@@ -26,5 +39,5 @@ export async function OnboardingAction(prevState: any, formData: FormData) {
         },
     });
 
-
+    return redirect("/dashboard");
 }
