ccb34d858be9baebedf00813179419378e148cd9
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-21 08:46:30 +0100

    Update onboarding flow to redirect to grant ID page and enhance user data retrieval

--- app/actions.ts
+++ app/actions.ts
@@ -39,5 +39,5 @@ export async function OnboardingAction(prevState: any, formData: FormData) {
         },
     });
 
-    return redirect("/dashboard");
+    return redirect("/onboarding/grant-id");
 }
