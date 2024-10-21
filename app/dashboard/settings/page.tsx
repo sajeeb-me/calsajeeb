2fce580e949686023cba8c0eb0e600fb20e1b7ec
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-21 09:34:26 +0100

    Add user data retrieval function in settings page with not found handling

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,4 +1,25 @@
 import { SettingsForm } from "@/app/components/SettingsForm";
+import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
+
+async function getData(id: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            name: true,
+            email: true,
+            image: true,
+        },
+    });
+
+    if (!data) {
+        return notFound();
+    }
+
+    return data;
+}
 
 export default function SettingsRoute() {
     return (
