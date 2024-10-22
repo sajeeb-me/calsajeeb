fb1a16818927d6249773b53b87ab9b31f8e93fe3
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-22 10:55:48 +0100

    Add default availability schedule for onboarding action

--- app/actions.ts
+++ app/actions.ts
@@ -37,6 +37,47 @@ export async function OnboardingAction(prevState: any, formData: FormData) {
         data: {
             userName: submission.value.userName,
             name: submission.value.fullName,
+            Availability: {
+                createMany: {
+                    data: [
+                        {
+                            day: "Monday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Tuesday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Wednesday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Thursday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Friday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Saturday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                        {
+                            day: "Sunday",
+                            fromTime: "08:00",
+                            tillTime: "18:00",
+                        },
+                    ]
+                }
+            }
         },
     });
 
