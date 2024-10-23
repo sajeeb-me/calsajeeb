12e3d6b14468609a6cf1edf11112405dbe97a2cb
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-23 09:26:35 +0100

    Add eventTypeSchema for event creation validation

--- app/lib/zodSchemas.ts
+++ app/lib/zodSchemas.ts
@@ -40,3 +40,11 @@ export const aboutSettingsSchema = z.object({
 
     profileImage: z.string(),
 });
+
+export const eventTypeSchema = z.object({
+    title: z.string().min(3).max(150),
+    duration: z.number().min(1).max(100),
+    url: z.string().min(3).max(150),
+    description: z.string().min(3).max(300),
+    videoCallSoftware: z.string(),
+});
