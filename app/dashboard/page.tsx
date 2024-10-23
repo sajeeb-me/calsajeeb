a92e46a2a552b7cd3bcad649c0dacd9aa6554e65
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-23 10:27:48 +0100

    Add header and button for creating new event types in DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation";
 import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
 import { EmptyState } from "../components/EmptyState";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 async function getData(userId: string) {
     const data = await prisma.user.findUnique({
@@ -35,6 +37,17 @@ export default async function DashboardPage() {
 
     return (
         <>
+            <div className="flex items-center justify-between px-2">
+                <div className="sm:grid gap-1 hidden">
+                    <h1 className="font-heading text-3xl md:text-4xl">Event Types</h1>
+                    <p className="text-lg text-muted-foreground">
+                        Create and manage your event types.
+                    </p>
+                </div>
+                <Button asChild>
+                    <Link href="/dashboard/new">Create New Event</Link>
+                </Button>
+            </div>
             {data.eventType.length === 0 ? (
                 <EmptyState
                     title="You have no Event Types"
@@ -43,7 +56,7 @@ export default async function DashboardPage() {
                     href="/dashboard/new"
                 />
             ) : (
-                <p>we have data</p>
+                <div>we have data</div>
             )}
         </>
     )
