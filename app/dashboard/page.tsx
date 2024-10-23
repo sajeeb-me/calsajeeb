7b11e6d0f8830d38a3f6270ff25a1db9c20647fb
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-23 10:48:10 +0100

    Add dropdown menu for event actions in DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,9 @@ import { notFound } from "next/navigation";
 import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
 import { EmptyState } from "../components/EmptyState";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Users2 } from "lucide-react";
+import { ExternalLink, Link2, Pen, Settings, Trash, Users2 } from "lucide-react";
 import Link from "next/link";
 import { Switch } from "@/components/ui/switch";
 
@@ -64,6 +65,44 @@ export default async function DashboardPage() {
                             className="overflow-hidden shadow rounded-lg border relative"
                             key={item.id}
                         >
+                            <div className="absolute top-2 right-2">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="outline" size="icon">
+                                            <Settings className="size-4" />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="w-20" align="end">
+                                        <DropdownMenuLabel>Event</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuGroup>
+                                            <DropdownMenuItem asChild>
+                                                <Link href={`/${data.userName}/${item.url}`}>
+                                                    <ExternalLink className="mr-2 h-4 w-4" />
+                                                    <span>Preview</span>
+                                                </Link>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                <Link2 className="mr-2 size-4" />
+                                                <span>Copy</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem asChild>
+                                                <Link href={`/dashboard/event/${item.id}`}>
+                                                    <Pen className="mr-2 h-4 w-4" />
+                                                    <span>Edit</span>
+                                                </Link>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuGroup>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem asChild>
+                                            <Link href={`/dashboard/event/${item.id}/delete`}>
+                                                <Trash className="mr-2 h-4 w-4" />
+                                                <span>Delete</span>
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
 
                             <Link href={`/dashboard/event/${item.id}`}>
                                 <div className="p-5">
