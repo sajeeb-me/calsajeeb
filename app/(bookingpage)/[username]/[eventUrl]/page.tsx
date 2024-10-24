ba2c7a9d33a2707d34d6341e4adf895c06df5f16
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-24 11:35:08 +0100

    Add form for booking meetings and update event details display in BookingFormRoute

--- app/(bookingpage)/[username]/[eventUrl]/page.tsx
+++ app/(bookingpage)/[username]/[eventUrl]/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/app/lib/db";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { BookMarked, CalendarX2, Clock } from "lucide-react";
+import { CalendarX2, Clock, VideoIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -62,10 +62,11 @@ export default async function BookingFormRoute({
         month: "long",
     }).format(selectedDate);
 
+    // const showForm = !!searchParams.date && !!searchParams.time;
 
     return (
         <div className="min-h-screen w-screen flex items-center justify-center">
-            <Card className="max-w-[600px]">
+            {/* <Card className="max-w-[600px]">
                 <CardContent className="p-5 grid md:grid-cols-[1fr,auto,1fr] gap-4">
                     <div>
                         <Image
@@ -97,13 +98,85 @@ export default async function BookingFormRoute({
                                 </span>
                             </p>
                             <p className="flex items-center">
-                                <BookMarked className="size-4 mr-2 text-primary" />
+                                <VideoIcon className="size-4 mr-2 text-primary" />
                                 <span className="text-sm font-medium text-muted-foreground">
                                     {eventType.videoCallSoftware}
                                 </span>
                             </p>
                         </div>
                     </div>
+                    <Separator
+                        orientation="vertical"
+                        className="hidden md:block h-full w-[1px]"
+                    />
+
+                    <form
+                        className="flex flex-col gap-y-4"
+                        action={createMeetingAction}
+                    >
+                        <input type="hidden" name="eventTypeId" value={eventType.id} />
+                        <input type="hidden" name="username" value={params.username} />
+                        <input type="hidden" name="fromTime" value={searchParams.time} />
+                        <input type="hidden" name="eventDate" value={searchParams.date} />
+                        <input
+                            type="hidden"
+                            name="meetingLength"
+                            value={eventType.duration}
+                        />
+                        <div className="flex flex-col gap-y-1">
+                            <Label>Your Name</Label>
+                            <Input name="name" placeholder="Your Name" />
+                        </div>
+
+                        <div className="flex flex-col gap-y-1">
+                            <Label>Your Email</Label>
+                            <Input name="email" placeholder="johndoe@gmail.com" />
+                        </div>
+
+                        <SubmitButton text="Book Meeting" />
+                    </form>
+                </CardContent>
+            </Card> */}
+
+            <Card className="w-full max-w-[1000px] mx-auto">
+                <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr] md:gap-4">
+                    <div>
+                        <Image
+                            src={eventType.user?.image as string}
+                            alt={`${eventType.user?.name}'s profile picture`}
+                            className="size-9 rounded-full"
+                            width={30}
+                            height={30}
+                        />
+                        <p className="text-sm font-medium text-muted-foreground mt-1">
+                            {eventType.user?.name}
+                        </p>
+                        <h1 className="text-xl font-semibold mt-2">{eventType.title}</h1>
+                        <p className="text-sm font-medium text-muted-foreground">
+                            {eventType.description}
+                        </p>
+                        <div className="mt-5 grid gap-y-3">
+                            <p className="flex items-center">
+                                <CalendarX2 className="size-4 mr-2 text-primary" />
+                                <span className="text-sm font-medium text-muted-foreground">
+                                    {formattedDate}
+                                </span>
+                            </p>
+                            <p className="flex items-center">
+                                <Clock className="size-4 mr-2 text-primary" />
+                                <span className="text-sm font-medium text-muted-foreground">
+                                    {eventType.duration} Mins
+                                </span>
+                            </p>
+                            <p className="flex items-center">
+                                <VideoIcon className="size-4 mr-2 text-primary" />
+                                <span className="text-sm font-medium text-muted-foreground">
+                                    Google Meet
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+
                     <Separator
                         orientation="vertical"
                         className="hidden md:block h-full w-[1px]"
