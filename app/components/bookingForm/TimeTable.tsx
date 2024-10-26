65d37d259cca0d0282452879e7b7ae525b98c894
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-26 11:04:51 +0100

    Add availability fetching to TimeTable component based on selectedDate and userName

--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,3 +1,5 @@
+import prisma from "@/app/lib/db";
+import { Prisma } from "@prisma/client";
 import { format } from "date-fns";
 
 
@@ -7,12 +9,43 @@ interface iappProps {
     meetingDuration: number;
 }
 
+async function getAvailability(selectedDate: Date, userName: string) {
+    const currentDay = format(selectedDate, "EEEE");
 
-export function TimeTable({
+    const startOfDay = new Date(selectedDate);
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date(selectedDate);
+    endOfDay.setHours(23, 59, 59, 999);
+    const data = await prisma.availability.findFirst({
+        where: {
+            day: currentDay as Prisma.EnumDayFilter,
+            User: {
+                userName: userName,
+            },
+        },
+        select: {
+            fromTime: true,
+            tillTime: true,
+            id: true,
+            User: {
+                select: {
+                    grantEmail: true,
+                    grantId: true,
+                },
+            },
+        },
+    });
+
+    return data;
+}
+
+export async function TimeTable({
     selectedDate,
     userName,
     meetingDuration,
 }: iappProps) {
+    const data = await getAvailability(selectedDate, userName);
+
     return (
         <div>
             <p className="text-base font-semibold">
