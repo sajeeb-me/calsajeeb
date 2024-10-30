76338ad3ccee7828e617c663278e47d2ad9a19e6
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-30 20:25:50 +0000

    Add DeleteEventType component for event type deletion confirmation

---
+++ app/dashboard/event/[eventTypeId]/delete/page.tsx
@@ -0,0 +1,12 @@
+export default function DeleteEventType() {
+    return (
+        <div>
+            <h1>Delete Event Type</h1>
+            <p>Are you sure you want to delete this event type?</p>
+            <div>
+                <button>Delete</button>
+                <button>Cancel</button>
+            </div>
+        </div>
+    )
+}
