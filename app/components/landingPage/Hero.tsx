b4d7c0bf5cef43db41513d55257c7c880b7f0fa1
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-31 20:34:33 +0000

    Update Hero component text for improved clarity and engagement

--- app/components/landingPage/Hero.tsx
+++ app/components/landingPage/Hero.tsx
@@ -13,13 +13,12 @@ export function Hero() {
                     </span>
 
                     <h1 className="mt-8 text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-medium leading-none">
-                        Scheduling made{" "}
-                        <span className="block text-primary">super easy!</span>
+                        Seamless scheduling{" "}
+                        <span className="block text-primary">at your fingertips!</span>
                     </h1>
 
-                    <p className="max-w-xl mx-auto mt-4   lg:text-lg text-muted-foreground ">
-                        Scheduling a meeting can be a pain. But we at CalSajeeb make it
-                        easy for your clients to schedule meetings with you.
+                    <p className="max-w-xl mx-auto mt-4 lg:text-lg text-muted-foreground">
+                        Simplify your scheduling process with CalSajeeb, making it effortless for clients to book meetings with you.
                     </p>
                     <div className=" mt-5 mb-12">
                         <AuthModal />
