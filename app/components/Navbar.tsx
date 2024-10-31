a45273b487b63af90156e5409890ca86a0ad1993
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-31 17:10:42 +0000

    Refactor Navbar component for improved layout and responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/logo.png";
-
 import { AuthModal } from "./AuthModal";
 import { ThemeToggle } from "./ThemeToggle";
+
 export function Navbar() {
     return (
         <div className="relative flex flex-col w-full py-5 mx-auto md:flex-row md:items-center md:justify-between">
             <div className="flex flex-row items-center justify-between text-sm lg:justify-start">
-                <Link href="/" className="flex items-center gap-2">
-                    <Image src={Logo} className="size-10" alt="Logo" />
+                <Link href="/" className="flex items-center gap-0.5 md:gap-2">
+                    <Image src={Logo} className="size-5 md:size-10" alt="Logo" />
 
-                    <h4 className="text-3xl font-semibold">
+                    <h4 className="text-base md:text-3xl font-semibold">
                         Cal<span className="text-primary">Sajeeb</span>
                     </h4>
                 </Link>
