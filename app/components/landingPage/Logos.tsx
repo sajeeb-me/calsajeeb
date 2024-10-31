50e273241b84bf3569513df3536a0b3d69cd992f
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-10-31 20:28:56 +0000

    Add Marquee component for logo display and update dependencies

--- app/components/landingPage/Logos.tsx
+++ app/components/landingPage/Logos.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import NylasLogo from "@/public/nylas-logo.png";
 import NextjsLogo from "@/public/nextjs-logo.svg";
 import vercelLogo from "@/public/vercel.svg";
+import Marquee from "react-fast-marquee";
 
 export function Logos() {
     return (
@@ -9,33 +10,35 @@ export function Logos() {
             <h2 className="text-center text-lg font-semibold leading-7">
                 Trusted by the best companies in the world
             </h2>
-            <div className="mt-10 grid max-w-lg mx-auto grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                <Image
-                    src={NylasLogo}
-                    alt="Logo"
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                />
-                <Image
-                    src={NextjsLogo}
-                    alt="Logo"
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                />
-                <Image
-                    src={vercelLogo}
-                    alt="Logo"
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                />
-                <Image
-                    src={NylasLogo}
-                    alt="Logo"
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                />
-                <Image
-                    src={NextjsLogo}
-                    alt="Logo"
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                />
-            </div>
+            <Marquee>
+                <div className="mt-10 max-w-lg mx-auto sm:max-w-x lg:mx-0 lg:max-w-none flex gap-x-14 overflow-hidden">
+                    <Image
+                        src={NylasLogo}
+                        alt="Logo"
+                        className="col-span-2 max-h-7 md:max-h-12 w-full object-contain lg:col-span-1 dark:invert"
+                    />
+                    <Image
+                        src={NextjsLogo}
+                        alt="Logo"
+                        className="col-span-2 max-h-7 md:max-h-12 w-full object-contain lg:col-span-1 dark:invert"
+                    />
+                    <Image
+                        src={vercelLogo}
+                        alt="Logo"
+                        className="col-span-2 max-h-7 md:max-h-12 w-full object-contain lg:col-span-1 dark:invert"
+                    />
+                    <Image
+                        src={NylasLogo}
+                        alt="Logo"
+                        className="col-span-2 max-h-7 md:max-h-12 w-full object-contain lg:col-span-1 dark:invert"
+                    />
+                    <Image
+                        src={NextjsLogo}
+                        alt="Logo"
+                        className="col-span-2 max-h-7 md:max-h-12 w-full object-contain lg:col-span-1 dark:invert"
+                    />
+                </div>
+            </Marquee>
         </div>
     );
 }
