d5a2e737725ea585951fab8321729276b3f4697a
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-11-01 18:28:38 +0000

    Replace Ban icon with Frown in EmptyState component for improved visual feedback

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Ban, PlusCircle } from "lucide-react";
+import { Ban, Frown, PlusCircle } from "lucide-react";
 import Link from "next/link";
 
 interface iAppProps {
@@ -17,8 +17,9 @@ export function EmptyState({
 }: iAppProps) {
     return (
         <div className="flex flex-col flex-1 h-full items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
-            <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
-                <Ban className="size-10 text-primary" />
+            <div className="flex size-32 items-center justify-center rounded-full bg-primary/10">
+                {/* <Ban className="size-10 text-primary" /> */}
+                <Frown className="size-20 text-primary" />
             </div>
             <h2 className="mt-6 text-xl font-semibold">{title}</h2>
             <p className="mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto">
